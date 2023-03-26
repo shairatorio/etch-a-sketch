b23510a95aad1ffc0741a4afc4f51f8620520496
shairatorio <shairatorio08@gmail.com>
2023-03-27 00:33:49 +0800

    Add functionality of Grid and Slider Label

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-const container = document.getElementById("grid");
+const grid = document.getElementById("grid");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (i = 0; i < (rows * cols); i++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+const sliderLabel = document.querySelector(".slider-label");
+let slider = document.querySelector(".slider");
+
+const gridItem = getComputedStyle(document.documentElement)
+.getPropertyValue('--color-light-gray');
+
+//createGrid
+function createGrid(size) {
+  for (let i = 1; i <= size * size; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.style.width = `calc(100% / ${size})`;
+  cell.style.height = `calc(100% / ${size})`;
+  cell.style.border = '1px solid '+ gridItem;
+
+  grid.appendChild(cell);
   };
 };
 
-makeRows(16, 16);
+// sliderLabel
+function updateSliderLabel() {
+  sliderLabel.innerText = `${slider.value} x ${slider.value}`;
+};
+
+// colorPicker
+// solidColor
+// mixColor
+// erase
+// clear
+
+slider.addEventListener("input", (e) => updateSliderLabel(e.target.value));
+
+window.onload = () => {
+  createGrid(16);
+};
