0455e58f6fd17c5c8764ac926001b4f32e0d59e6
shairatorio <shairatorio08@gmail.com>
2023-03-28 00:22:40 +0800

    Add functionality of grid size & button effects

--- script.js
+++ script.js
@@ -1,37 +1,71 @@
-const grid = document.getElementById("grid");
+let grid = document.getElementById("grid");
+let gridSize = 16;
 
-const sliderLabel = document.querySelector(".slider-label");
-let slider = document.querySelector(".slider");
+let colorPicker = document.getElementById("colorPicker");
+let btnPencil =  document.getElementById("btnPencil");
+let btnRainbow =  document.getElementById("btnRainbow");
+let btnErase =  document.getElementById("btnErase");
+let btnClear =  document.getElementById("btnClear");
+let sliderLabel = document.getElementById("sliderLabel");
+let slider = document.getElementById("slider");
 
-const gridItem = getComputedStyle(document.documentElement)
-.getPropertyValue('--color-light-gray');
+let gridBorderColor = getComputedStyle(document.documentElement).getPropertyValue('--color-light-gray');
 
-//createGrid
 function createGrid(size) {
   for (let i = 1; i <= size * size; i++) {
-  const cell = document.createElement("div");
+  let cell = document.createElement("div");
   cell.classList.add("cell");
   cell.style.width = `calc(100% / ${size})`;
   cell.style.height = `calc(100% / ${size})`;
-  cell.style.border = '1px solid '+ gridItem;
+  cell.style.border = '1px solid '+ gridBorderColor;
 
   grid.appendChild(cell);
   };
 };
 
-// sliderLabel
+function updateSizeGrid(size) {
+  grid.innerHTML = '';
+  createGrid(size);
+}
+
 function updateSliderLabel() {
   sliderLabel.innerText = `${slider.value} x ${slider.value}`;
 };
 
 // colorPicker
-// solidColor
-// mixColor
-// erase
+
+function clickBtnPencil() {
+  removeActiveButtons();
+
+  btnPencil.classList.add("active-button");
+}
+
+function clickBtnRainbow() {
+  removeActiveButtons();
+
+  btnRainbow.classList.add("active-button");
+}
+
+function clickBtnErase() {
+  removeActiveButtons();
+
+  btnErase.classList.add("active-button");
+}
+
+function removeActiveButtons() {
+  btnPencil.classList.remove("active-button");
+  btnRainbow.classList.remove("active-button");
+  btnErase.classList.remove("active-button");
+}
+
 // clear
 
 slider.addEventListener("input", (e) => updateSliderLabel(e.target.value));
+slider.addEventListener("input", (e) => updateSizeGrid(e.target.value));
+btnPencil.addEventListener("click", (e) => clickBtnPencil(e.target.value));
+btnRainbow.addEventListener("click", (e) => clickBtnRainbow(e.target.value));
+btnErase.addEventListener("click", (e) => clickBtnErase(e.target.value));
 
 window.onload = () => {
-  createGrid(16);
+  createGrid(gridSize);
 };
