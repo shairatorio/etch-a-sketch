165812891f302861f6b81be580c6a703e2ef4f3d
shairatorio <shairatorio08@gmail.com>
2023-04-26 18:29:25 +0800

    Fix bugs

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-let grid = document.querySelector("#grid");
+let grid = document.querySelector('#grid');
 let gridSize = 16;
-let colorPicker = document.querySelector("#colorPicker");
-let btnPencil =  document.querySelector("#btnPencil");
-let btnRainbow =  document.querySelector("#btnRainbow");
-let btnErase =  document.querySelector("#btnErase");
-let btnClear =  document.querySelector("#btnClear");
-let sliderLabel = document.querySelector("#sliderLabel");
-let slider = document.querySelector("#slider");
-let leftRoller = document.querySelector(".left-roller");
-let rightRoller = document.querySelector(".right-roller");
+let colorPicker = document.querySelector('#colorPicker');
+let btnPencil =  document.querySelector('#btnPencil');
+let btnRainbow =  document.querySelector('#btnRainbow');
+let btnErase =  document.querySelector('#btnErase');
+let btnClear =  document.querySelector('#btnClear');
+let sliderLabel = document.querySelector('#sliderLabel');
+let slider = document.querySelector('#slider');
+let leftRoller = document.querySelector('.left-roller');
+let rightRoller = document.querySelector('.right-roller');
 let gridBorderColor = getComputedStyle(document.documentElement).getPropertyValue('--color-light-gray');
 let eraseColor = getComputedStyle(document.documentElement).getPropertyValue('--color-white');
 const percentage = 100;
-let boolCPS = false;
 
 function createGrid(size) {
   for (let i = 1; i <= size * size; i++) {
-  let cell = document.createElement("div");
-  cell.classList.add("cell");
+  let cell = document.createElement('div');
+  cell.classList.add('cell');
   cell.style.width = `calc(${percentage}% / ${size})`;
   cell.style.height = `calc(${percentage}% / ${size})`;
   cell.style.border = '1px solid '+ gridBorderColor;
@@ -27,10 +26,6 @@ function createGrid(size) {
 
 function updateSizeGrid(size) {
   grid.innerHTML = '';
-  // if (boolCPS === true) {
-  //   preSelectedButton();
-  //   return
-  // }
   createGrid(size);
 }
 
@@ -38,55 +33,42 @@ function updateSliderLabel() {
   sliderLabel.innerText = `${slider.value} x ${slider.value}`;
 };
 
-function preSelectedButton() { 
-  boolCPS = false;
-  btnPencil.focus();
-  addGlobalEventListener("mouseover", ".cell", e => {
-    btnPencil.focus();
-    e.target.style.background = colorPicker.value;
-  });
-}
-
 function clickButton(btnType) {
   switch (btnType) {
-    case 'pencil':      
-      preSelectedButton();
-    break;
+    case 'pencil':
+    case 'color-picker':
+    case 'slider':
+      btnPencil.focus();
+      break;
     case 'rainbow':
-      addGlobalEventListener("mouseover", ".cell", e => {
-        if (boolCPS === true) {
-          console.log('true');
-          preSelectedButton();
-          return
-        } else {
-          console.log('false');
-          boolCPS = false;
-          btnRainbow.focus();
-          e.target.style.background = randomColor();
-        }
-      });
-    break;
+      btnRainbow.focus();
+      break;
     case 'erase':
-      addGlobalEventListener("mouseover", ".cell", e => {
-        if (boolCPS === true) {
-          preSelectedButton();
-          return
-        } else {
-          boolCPS = false;
-          btnErase.focus();
-          e.target.style.background = eraseColor;
-        }
-      });
-    break;
+      btnErase.focus();
+      break;
     case 'clear':
       const cells = document.querySelectorAll('.cell');
       cells.forEach((cell) => {
         cell.style.background = eraseColor;
       });
-      preSelectedButton();
-    break;
+      btnPencil.focus();
+      break;
   }
 }
+
+addGlobalEventListener("mouseover", ".cell", e => {
+  switch (document.activeElement.id) {
+    case 'btnPencil':
+      e.target.style.background = colorPicker.value;
+      break;
+    case 'btnRainbow':
+      e.target.style.background = randomColor();
+      break;
+    case 'btnErase':
+      e.target.style.background = eraseColor;
+      break;
+  }
+});
 
 function randomColor() {
   let color = '#';
@@ -98,8 +80,8 @@ function randomColor() {
 }
 
 function rotateRollers() {
-  leftRoller.classList.toggle("rotate");
-  rightRoller.classList.toggle("rotate");
+  leftRoller.classList.toggle('rotate');
+  rightRoller.classList.toggle('rotate');
 }
 
 function addGlobalEventListener(type, selector, callback) {
@@ -108,16 +90,16 @@ function addGlobalEventListener(type, selector, callback) {
   })
 }
 
-addGlobalEventListener("click", "button", e => clickButton(e.target.classList[0]));
-addGlobalEventListener("mousemove", ".cell", () => rotateRollers());
-addGlobalEventListener("click", ".color-picker", e => e.type === 'click' ? boolCPS = true : null);
-addGlobalEventListener("change", ".slider", e => {
-  // boolCPS = true;
+addGlobalEventListener('click', 'button', e => clickButton(e.target.classList[0]));
+addGlobalEventListener('mousemove', '.cell', () => rotateRollers());
+addGlobalEventListener('change', '.color-picker', e => clickButton(e.target.classList[0]));
+addGlobalEventListener('change', '.slider', e => {
   updateSliderLabel(e.target.value);
   updateSizeGrid(e.target.value);
+  clickButton(e.target.classList[0]);
 });
 
 window.onload = () => {
   createGrid(gridSize);
-  preSelectedButton();
+  btnPencil.focus();
 };
